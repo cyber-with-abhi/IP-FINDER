e1c840e2a10bce928c45d9c33e2b7b612905f530
cyber-with-abhi <m.abhishek14677@gmail.com>
2025-10-21 10:48:44 +0530

    Add files via upload

---
+++ IP FINDER.py
@@ -0,0 +1,9 @@
+import socket
+
+def get_ip_address(url):
+    try:
+        ip_address = socket.gethostbyname(url)
+        return ip_address
+    except socket.error as err:
+        return f"Error: {err}"
+print(f"The IP address of {url} is: {get_ip_address(url)}")
